63e53d94d440d5eb9afca614a688c7a8930b24a7
StivMore <IngenieroStiven@outlook.es>
2022-01-05 17:37:47 -0500

    local

---
+++ lib/Views/Components/player_widget.dart
@@ -0,0 +1,197 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_exoplayer/audioplayer.dart';
+import 'package:spotify/Views/Utils/Responsive/responsive.dart';
+
+class PlayerWidget extends StatefulWidget {
+  final String url;
+  final AudioPlayer audioPlayer;
+  final bool? isUser;
+
+  PlayerWidget({
+    required this.url,
+    required this.audioPlayer,
+    this.isUser,
+  });
+
+  @override
+  State<StatefulWidget> createState() {
+    return _PlayerWidgetState(url: url, audioPlayer: audioPlayer);
+  }
+}
+
+class _PlayerWidgetState extends State<PlayerWidget> {
+  final String? url;
+  AudioPlayer? audioPlayer;
+
+  Duration? _duration;
+  Duration? _position;
+
+  PlayerState _playerState = PlayerState.RELEASED;
+  StreamSubscription? _durationSubscription;
+  StreamSubscription? _positionSubscription;
+  StreamSubscription? _playerCompleteSubscription;
+  StreamSubscription? _playerErrorSubscription;
+  StreamSubscription? _playerStateSubscription;
+  StreamSubscription? _playerIndexSubscription;
+  StreamSubscription? _playerAudioSessionIdSubscription;
+
+  get _durationText => _duration?.toString().split('.').first ?? '';
+  get _positionText => _position?.toString().split('.').first ?? '';
+
+  _PlayerWidgetState({this.url, this.audioPlayer});
+
+  @override
+  void initState() {
+    super.initState();
+    _initAudioPlayer();
+  }
+
+  @override
+  void dispose() {
+    audioPlayer!.dispose();
+    _durationSubscription?.cancel();
+    _positionSubscription?.cancel();
+    _playerCompleteSubscription?.cancel();
+    _playerErrorSubscription?.cancel();
+    _playerStateSubscription?.cancel();
+    _playerIndexSubscription?.cancel();
+    _playerAudioSessionIdSubscription?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Responsive responsive = Responsive(context);
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      _positionText == ''
+                          ? ''
+                          : '${_positionText[3]}:${_positionText[5] + _positionText[6] ?? ""}',
+                      style: const TextStyle(fontSize: 15, color: Colors.white),
+                    ),
+                    Text(
+                      _durationText == ''
+                          ? ''
+                          : '${_durationText[3]}:${_durationText[5] + _durationText[6] ?? ''}',
+                      style: const TextStyle(fontSize: 15, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+                width: responsive.wp(90),
+                child: SliderTheme(
+                  data: const SliderThemeData(
+                    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 5),
+                    trackHeight: 3,
+                    thumbColor: Colors.white,
+                    inactiveTrackColor: Colors.grey,
+                    activeTrackColor: Colors.white,
+                    overlayColor: Colors.transparent,
+                  ),
+                  child: Slider(
+                    value: _position?.inMilliseconds.toDouble() ?? 0.0,
+                    min: 0.0,
+                    max: _duration?.inMilliseconds.toDouble() ?? 0.0,
+                    onChanged: (double value) async {
+                      final Result result = await audioPlayer!
+                          .seekPosition(Duration(milliseconds: value.toInt()));
+                      if (result == Result.FAIL) {}
+                      _position = Duration(milliseconds: value.toInt());
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            height: 70,
+            width: 70,
+            decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                    colors: [Color(0xff7A7B8C), Color(0xff4D4E65)],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    transform: GradientRotation(0.7853982)),
+                borderRadius: BorderRadius.circular(48)),
+            child: Center(
+              child: IconButton(
+                  onPressed: () async {
+                    if (_playerState == PlayerState.PLAYING) {
+                      audioPlayer!.pause().whenComplete(() {});
+                    } else {
+                      if (_duration != null) {
+                        if (_playerState == PlayerState.PAUSED) {
+                          await audioPlayer!.resume();
+                        } else {
+                          await audioPlayer!.play(
+                            url!,
+                          );
+                        }
+                      } else {
+                        Result resp = await audioPlayer!.play(
+                          url!,
+                        );
+                        if (resp == Result.SUCCESS) {}
+                      }
+                    }
+                  },
+                  iconSize: 26.0,
+                  icon: _playerState == PlayerState.RELEASED
+                      ? const Icon(Icons.play_arrow)
+                      : _playerState == PlayerState.PLAYING
+                          ? const Icon(Icons.pause)
+                          : const Icon(Icons.play_arrow),
+                  color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _initAudioPlayer() {
+    audioPlayer = AudioPlayer();
+    _durationSubscription = audioPlayer!.onDurationChanged.listen((duration) {
+      setState(() {
+        _duration = duration;
+      });
+    });
+    _positionSubscription = audioPlayer!.onAudioPositionChanged.listen((pos) {
+      setState(() {
+        _position = pos;
+      });
+    });
+    _playerStateSubscription =
+        audioPlayer!.onPlayerStateChanged.listen((playerState) {
+      setState(() {
+        _playerState = playerState;
+      });
+    });
+    _playerIndexSubscription =
+        audioPlayer!.onCurrentAudioIndexChanged.listen((index) {
+      setState(() {
+        _position = const Duration(milliseconds: 0);
+      });
+    });
+    _playerAudioSessionIdSubscription =
+        audioPlayer!.onAudioSessionIdChange.listen((audioSessionId) {});
+    _playerCompleteSubscription = audioPlayer!.onPlayerCompletion.listen((a) {
+      _position = const Duration(milliseconds: 0);
+    });
+  }
+}
--- lib/Views/Home/Details/DetailTrack.dart
+++ lib/Views/Home/Details/DetailTrack.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_exoplayer/audioplayer.dart';
 import 'package:spotify/Domain/Models/Stander/tracksModel.dart';
 import 'package:provider/provider.dart';
 import 'package:spotify/Domain/Bloc/SpotifyBloc.dart';
-import 'package:spotify/Views/Utils/Animations/FadeAnimation.dart';
+import 'package:spotify/Views/Components/player_widget.dart';
 import 'package:spotify/Views/Utils/Responsive/responsive.dart';
 
 class DetailTrack extends StatefulWidget {
+  final String? playlist;
   final TrackModel? trackModel;
-  DetailTrack({Key? key, this.trackModel}) : super(key: key);
+  const DetailTrack({Key? key, this.playlist, this.trackModel})
+      : super(key: key);
 
   @override
   _DetailTrackState createState() => _DetailTrackState();
@@ -15,6 +18,7 @@ class DetailTrack extends StatefulWidget {
 
 class _DetailTrackState extends State<DetailTrack> {
   final ScrollController scrollController = ScrollController();
+  AudioPlayer audioPlayer = AudioPlayer();
   @override
   void initState() {
     super.initState();
@@ -32,7 +36,7 @@ class _DetailTrackState extends State<DetailTrack> {
       appBar: AppBar(
         backgroundColor: const Color(0xff212121),
         title: Text(
-          widget.trackModel!.name!,
+          widget.playlist!,
           style: const TextStyle(color: Colors.white),
         ),
         centerTitle: true,
@@ -40,149 +44,78 @@ class _DetailTrackState extends State<DetailTrack> {
       backgroundColor: const Color(0xff212121),
       body: WillPopScope(
         onWillPop: () async {
-          spotifybloc.clearOPTTrack();
           return Future.value(true);
         },
         child: SizedBox(
           height: responsive.height,
           width: responsive.width,
-          child: Column(
-            children: [
-              Container(
-                height: 310,
-                width: responsive.width,
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 8,
-                        child: spotifybloc.globalstates == true
-                            ? const LinearProgressIndicator(
-                                color: Colors.white,
-                                backgroundColor: Color(0xff212121),
-                              )
-                            : null,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  widget.trackModel!.name!,
+                  style: const TextStyle(color: Colors.white, fontSize: 22),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  widget.trackModel!.artists![0].name!,
+                  style: TextStyle(
+                      color: Colors.white.withOpacity(0.7), fontSize: 16),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: SizedBox(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: FadeInImage.assetNetwork(
+                        placeholder: 'assets/no-image.jpg',
+                        image: widget.trackModel!.images![0].url!,
+                        fit: BoxFit.cover,
                       ),
-                      SizedBox(
-                        height: 249,
-                        width: responsive.width,
-                        child: ClipRRect(
-                          borderRadius: const BorderRadius.only(
-                              bottomLeft: Radius.circular(12),
-                              bottomRight: Radius.circular(12)),
-                          child: FadeInImage.assetNetwork(
-                            placeholder: 'assets/no-image.jpg',
-                            image: widget.trackModel!.images![0].url!,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Column(
-                            children: const [
-                              Text(
-                                'Tracks',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 24),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-              IgnorePointer(
-                ignoring: spotifybloc.globalstates,
-                child: SizedBox(
-                  height: (responsive.height - 404),
-                  width: responsive.width,
+                const SizedBox(
+                  height: 20,
+                ),
+                PlayerWidget(
+                    url: widget.trackModel!.previusURl!,
+                    audioPlayer: audioPlayer),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 100,
+                  width: responsive.wp(90),
                   child: ListView.builder(
-                      physics: const BouncingScrollPhysics(),
-                      controller: scrollController,
-                      itemCount: list.length,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: widget.trackModel!.images!.length,
                       itemBuilder: (context, index) {
-                        return GestureDetector(
-                          onTap: () async {},
-                          child: FadeAnimation(
-                            (1500 + (index * 5)),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                width: responsive.width,
-                                height: 90,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(16),
-                                  color: Colors.white.withOpacity(0.6),
-                                ),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(16),
-                                      child: FadeInImage.assetNetwork(
-                                        placeholder: 'assets/no-image.jpg',
-                                        image: list[index].images![0].url!,
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            list[index].name!.length > 20
-                                                ? '${list[index].name!.substring(0, 20)}...'
-                                                : '${list[index].name!}...',
-                                            style: TextStyle(
-                                                color: Colors.amber[400],
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 17),
-                                          ),
-                                          Text(
-                                            list[index].artists![0].name!.length >
-                                                    28
-                                                ? '${list[index].artists![0].name!.substring(0, 28)}...'
-                                                : '${list[index].artists![0].name!}...',
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                    Expanded(
-                                      child: IconButton(
-                                        onPressed: () {},
-                                        icon: Icon(
-                                          Icons.star,
-                                          color: Colors.amber[400],
-                                        ),
-                                        alignment: Alignment.centerRight,
-                                      ),
-                                    )
-                                  ],
-                                ),
+                        return Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: SizedBox(
+                            height: 100,
+                            width: 100,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(16),
+                              child: FadeInImage.assetNetwork(
+                                placeholder: 'assets/no-image.jpg',
+                                image: widget.trackModel!.images![index].url!,
+                                fit: BoxFit.cover,
                               ),
                             ),
                           ),
                         );
                       }),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
